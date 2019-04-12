d830c5d8a153d2bc5cacd944fa8244b98a8609b6
wei feng <theweifeng@gmail.com>
2019-04-12 02:13:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,14 @@
 def hello_t(array)
-  i = 0
   
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
-  
-  array
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    
+    array
 end
 
 # call your method here!
